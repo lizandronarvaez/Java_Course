06dc2667cd01ba7673a924f20e31aa255ddd8513
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2023-10-17 17:07:05 +0200

    Operadores de asignacion

---
+++ src/Operadores/Asignacion.java
@@ -0,0 +1,23 @@
+package Java_Curso.src.Operadores;
+
+public class Asignacion {
+    public static void main(String[] args) {
+        int i = 10;
+        // incremento
+        i += 4;
+        i++;
+
+        // decremeto
+        i -= 10;
+        i--;
+
+        // Multiplicacion
+        i *= 2;
+
+        // divison
+        i /= 4;
+
+        // Sout
+        System.out.println(i);
+    }
+}
