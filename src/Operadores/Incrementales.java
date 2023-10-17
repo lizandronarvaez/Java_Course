2d03f3055ca922c0cb7196a63fd81bde07d9e060
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2023-10-17 17:07:18 +0200

    operadores de incremento

---
+++ src/Operadores/Incrementales.java
@@ -0,0 +1,24 @@
+package Java_Curso.src.Operadores;
+
+public class Incrementales {
+    public static void main(String[] args) {
+        int i = 4;
+        int j = i;
+
+        // pre incremento
+        j = ++i;
+        // post increment
+        j = i++;
+
+        // Pre decremento
+        i= 3;
+        j=--i;
+
+        // post decremento
+        i=10;
+        j=i--;
+        //sout
+        System.out.println(i);
+        System.out.println(j);
+    }
+}
