14b74f24ede3155751554346e37f76e96f5c26af
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2023-10-27 00:07:37 +0200

    Argumentos lineas de comandos

---
+++ src/LineaComandos/ArgumentosLineaComandos.java
@@ -0,0 +1,19 @@
+package Java_Curso.src.LineaComandos;
+
+public class ArgumentosLineaComandos {
+
+    public static void main(String[] args) {
+
+        // System.exit(0) es que todod sale bien
+        // System.exit(1) se lanza una exepcion
+        // System.exit(-1) hay un error
+        if (args.length == 0) {
+            System.out.println("Debe ingresar argumentos o parámetros");
+            System.exit(-1);
+        }
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("Argumento nº" + i + " " + args[i]);
+        }
+
+    }
+}
