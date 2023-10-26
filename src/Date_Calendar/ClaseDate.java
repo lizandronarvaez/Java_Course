e9a5ce53944df53bce43ef35aaa07c82f0915af9
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2023-10-26 02:20:15 +0200

    Clase Calendar,manejo de fechas, formatos

--- src/Date_Calendar/ClaseDate.java
+++ src/Date_Calendar/ClaseDate.java
@@ -1,32 +1,44 @@
 package Java_Curso.src.Date_Calendar;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class ClaseDate {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
 
         Date fecha = new Date();
+        String fechaNacimiento = "24-12-1993";
+        Date calculaEdad;
+        // FOrmatea la fecha
+        SimpleDateFormat diffFecha = new SimpleDateFormat("dd-MM-yyyy");
+        calculaEdad = diffFecha.parse(fechaNacimiento);
 
-        // Modificar la fecha
-        SimpleDateFormat diffFecha = new SimpleDateFormat("EEEE dd 'de' MMMM 'del' yyyy");
+        // formateas las fechas al formato que quieres
+        diffFecha = new SimpleDateFormat("ddMMyyyy");
+        // obtienes la fecha en el forma que haz establecido
         String fechaString = diffFecha.format(fecha);
-
+        String fecha2 = diffFecha.format(calculaEdad);
+        int edad1=Integer.parseInt(fechaString);
+        int edad2=Integer.parseInt(fecha2);
+        System.out.println(edad1);
+        System.out.println(edad2);
         //
-        Long j = 0L;
-        for (int i = 0; i < 100; i++) {
-            j += i;
-        }
+        // Long j = 0L;
+        // for (int i = 0; i < 100; i++) {
+        // j += i;
+        // }
 
-        // Calcular el tiempo con getTime()
-        Date date = new Date();
-        long calcularTiempo = date.getTime() - fecha.getTime();
-        // Salida de datos
-        System.out.println(calcularTiempo + " milisegundos en realizar el ciclo for");
-        System.out.println("EL valor de j es : " + j);
-        System.out.println("La fecha de hoy: " + fecha);
-        System.out.println("Nuevo formato de fecha: " + fechaString);
+        // // Calcular el tiempo con getTime()
+        // Date date = new Date();
+        // long calcularTiempo = date.getTime() - fecha.getTime();
+        // // Salida de datos
+        // System.out.println(calcularTiempo + " milisegundos en realizar el ciclo
+        // for");
+        // System.out.println("EL valor de j es : " + j);
+        // System.out.println("La fecha de hoy: " + fecha);
+        // System.out.println("Nuevo formato de fecha: " + fechaString);
 
     }
 }
---
+++ src/Date_Calendar/JavaUtilDateParse.java
@@ -0,0 +1,48 @@
+package Java_Curso.src.Date_Calendar;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+public class JavaUtilDateParse {
+
+    public static void compararFecha() {
+
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");
+        System.out.println("Introduce una fecha con formato 'dd-MM-yyy': ");
+        try {
+            // Convertir una fecha string a objecto
+            Date fecha = format.parse(scanner.nextLine());
+            scanner.close();
+            System.out.println(fecha);
+            // Formato normal
+            // System.out.println(format.format(fecha));
+
+            // comparar una fecha
+            Date fecha2 = new Date();
+
+            if (fecha.after(fecha2)) {
+                System.out.println("Fecha del usuario es despues que fecha2");
+                return;
+            }
+            if (fecha.before(fecha2)) {
+                System.out.println("Fecha es anterior que fecha2");
+                return;
+            }
+
+            if (fecha.equals(fecha2)) {
+                System.out.println("Fecha es igual a fecha2");
+                return;
+            }
+        } catch (ParseException e) {
+
+            e.printStackTrace();
+        }
+    }
+}
